bce5d2e201f60b6913d477fe859fdcbdbe601fc4
ERama035 <erama035@gmail.com>
2016-01-11 15:21:59 -0800

    Update HelloWorldController.java

--- src/main/java/com/mkyong/common/controller/HelloWorldController.java
+++ src/main/java/com/mkyong/common/controller/HelloWorldController.java
@@ -16,30 +16,7 @@ public class HelloWorldController extends AbstractController {
 		model.addObject("msg", "hello world");
 
 		System.out.println("------");
-		System.out.println("***********");
-		
-		System.out.println("pusing to branch");
-		System.out.println("pusing to upstream");
-		
-			System.out.println("------");
-		System.out.println("***********");
-		
-		System.out.println("pusing to branch");
-		System.out.println("pusing to upstream");
-		
-		System.out.println("============================");
-		
-		
-		System.out.println("pusing to to to to to ");
-		
-
-			System.out.println(" ********hhhh***********test2 gfh****************------");
-			System.out.println(" *******************test2 gfhgfh 9999999**********-----");
 	
-		System.out.println("ppppppppppppppppppppppppppppppppppp");
-		System.out.println("ppppppppppppppppppppppppppppppppppp");
-		System.out.println("ppppppppppppppppppppppppppppppppppp ---sourve tree pull");
-		System.out.println("ppppppppppppppppppppppppppppppppppp ---sourve fetch");
 		return model;
 	}
 
