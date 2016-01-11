539c66bc435804afb11341eea646afd3e8305c74
ERama035 <erama035@gmail.com>
2016-01-11 10:19:45 -0800

    Update HelloWorldController.java

--- src/main/java/com/mkyong/common/controller/HelloWorldController.java
+++ src/main/java/com/mkyong/common/controller/HelloWorldController.java
@@ -33,7 +33,7 @@ public class HelloWorldController extends AbstractController {
 		System.out.println("pusing to to to to to ");
 		
 
-			System.out.println(" *******************test2 gfhgfh 999999999******uuuu**********************");
+			System.out.println(" *******************test2 gfhgfh 999999999******uuuu**********uuuuuu************");
 	
 		System.out.println("ppppppppppppppppppppppppppppppppppppppppppppppppppppppppp ");
 		return model;
